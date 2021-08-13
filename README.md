2f672e4734e5f2d8c24ac43ab3bf7f785d2da335
harsh4655 <83711793+harsh4655@users.noreply.github.com>
2021-08-13 19:05:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# PRO-35
+# MyVirtualPet
---
+++ sketch.js
@@ -0,0 +1,95 @@
+var dog,sadDog,happyDog,fedTime,lastFed,database;
+var foodS,foodStock;
+var feedPet,addDog;
+var foodObj,feed,addFood;
+foodS = 30;
+
+function preload(){
+  sadDog=loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+   database = firebase.database();
+   createCanvas(1000, 700);
+
+
+   foodObj = new Food();
+
+foodStock = database.ref('Food');
+foodStock.on("value",readStock);
+
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  feed = createButton("Feed the Dog")
+  feed.position(900,95);
+  feed.mousePressed(feedDog)
+ 
+  addFood = createButton("Add Food")
+ addFood.position(996,95);
+ addFood.mousePressed(addFoods);
+ 
+
+}
+
+function draw() {
+  background(46,139,87);
+  
+  foodObj.display();
+
+  fedTime = database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed = data.val();
+  })
+  
+  fill(255,255,254);
+  textSize(15)
+  if(lastFed>=12){
+    text("Last Feed : " + lastFed%12 + "PM",350,30);
+  }
+  else if(lastFed == 0){
+    text("Last Feed : 12 AM ",350,30);
+  }else{
+    text("Last Feed : " + lastFed + "AM ",350,30);
+  }
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+
+  foodS = data.val();
+  foodObj.updateFoodStock(foodS);
+  
+  }
+  
+
+//function to update food stock and last fed time
+function feedDog(){
+
+  dog.addImage(happyDog);
+
+var foodStockVal= foodObj.getFoodStock();
+if(foodStockVal<=0){
+  foodObj.updateFoodStock(foodStockVal*0);
+}else{
+  foodObj.updateFoodStock(foodStockVal-1)
+}
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
+  })
+
+}
+
+//function to add food in stock
+function addFoods(){
+  
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
